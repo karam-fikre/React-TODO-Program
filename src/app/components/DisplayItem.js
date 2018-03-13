af4a59905c1d0ba3bc26ebdadfc1e051db9c05e8
karam fikre <karam_fikre@hotmail.com>
2018-03-13 13:27:24 +0100

    Add the list and item display

---
+++ src/app/components/DisplayItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class DisplayItem extends React.Component{
+
+  render(){
+    var todo=this.props.todo;
+    var title=todo.title;
+    return <li>
+       <input
+       checked={todo.done}
+       onChange={this.props.handleDone.bind(null,title)}
+       type="checkbox"
+       style={{fontsize:'x-large'}}/>
+
+       {title}
+       <a href="#" onClick={this.props.handleDelete.bind(null,title)}>
+         [x]
+       </a>
+    </li>;
+  }
+}
